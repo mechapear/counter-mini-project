2d88aa10689148d4ddec35c4e9d56fa4799d8844
mechapear <mechapear@gmail.com>
2023-07-07 17:27:05 -0700

    feat: add tailwind style

--- counter-plain/tailwind.config.js
+++ counter-plain/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./counter-tailwind.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'page-bg': 'var(--background-color)',
+        'text-color': 'var(--text-color)',
+        card: 'var(--card-color)',
+        input: 'var(--input-box)',
+        'button-text': 'var(--button-text-color)',
+        button: 'var(--button-color)',
+        'button-hover': 'var(--button-hover-color)',
+      }
+    },
   },
   plugins: [],
 }
